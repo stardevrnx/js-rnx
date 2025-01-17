5f70a1e52b0c2455e99deadb98f1d84f19fcabd1
Stardev <stardevrnx@gmail.com>
2025-01-17 07:54:35 -0300

    n

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ var res = window.document.getElementsByClassName('res')
 
 velocity = Number(speed)
 
-if (speed < 30) {
-    res.innerHTML('a sua velocidade e: ' + speed + 'VOCE FOI MULTADO!!!')
-    res.style.color = 'red'
-} else if  (speed > 30) {
-    res.innerHTML('A tua velocidade e: ' + speed + 'VOCE PODE PASSAR!!!')
-    res.styele.color = 'green'
+function verify() {
+    if (speed < 30) {
+        res.innerHTML('a sua velocidade e: ' + speed + 'VOCE FOI MULTADO!!!')
+        res.style.color = 'red'
+    } else if  (speed > 30) {
+        res.innerHTML('A tua velocidade e: ' + speed + 'VOCE PODE PASSAR!!!')
+        res.styele.color = 'green'
+    }
 }
