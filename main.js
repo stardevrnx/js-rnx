744c9b5bddb9d14ce549f51df9bc3fcad48ad9fe
Stardev <stardevrnx@gmail.com>
2025-01-17 11:39:39 -0300

    n

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function verify() {
-    vel = querySelector('input#vel')
+    vel = window.document.getElementById('#vel')
 
     velocity = Number(vel.value)
 
     if (velocity < 30) {
         window.document.write("PARABENS!!!! VOCE FOI MULTADOOOOOO!!!!")
+    } else {
+        window.document.write("PARABEEEEEEENS!!! VOCE ESTA NA VELOCIDADE CORRETAAAAAA!!!")
     }
 }
