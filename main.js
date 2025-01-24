33fb8062f02160cdb9bacdac3c3a933994855666
Stardev <stardevrnx@gmail.com>
2025-01-24 17:59:15 -0300

    j

--- main.js
+++ main.js
@@ -4,5 +4,13 @@ function load() {
     var data = new Date()
     var hr = data.getHours()
 
-    msg.innerHTML = 'agora e ' + hr
+    msg.innerHTML = 'agora e ' + hr + 'horas'
+
+    if (hr >= 0 && hr < 12) {
+
+    } else if (hr >= 12 && hr < 18) {
+
+    } else {
+        
+    }
 }
