b5d18ea534959fdbe94c9a807cf3d3c506e59f58
Stardev <stardevrnx@gmail.com>
2025-02-27 15:34:22 -0400

    js file

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ function contar() {
    var fim = document.getElementById('txtf')
    var de_tantos = document.getElementById('txtd')
 
-   if(ini.Value.length == 0 || fim.Value.length == 0 || de_tantos.Value.length == 0) {
+   if (ini.Value.length == 0 || fim.Value.length == 0 || de_tantos.Value.length == 0) {
     window.alert("Invalid Value")
+   } else {
+    window.alert('ISSO AI GAROTAO')
    }
 
 }
