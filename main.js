2b8c42b6324f42d651562659caabcf97a284894b
Stardev <stardevrnx@gmail.com>
2025-02-27 15:32:19 -0400

    js file

---
+++ main.js
@@ -0,0 +1,10 @@
+function contar() {
+   var ini = document.getElementById('txti')
+   var fim = document.getElementById('txtf')
+   var de_tantos = document.getElementById('txtd')
+
+   if(ini.Value.length == 0 || fim.Value.length == 0 || de_tantos.Value.length == 0) {
+    window.alert("Invalid Value")
+   }
+
+}
